c9d148ba6e1b33f829e781f43d1f8225266fe004
Adrienne <amarshall813@gmail.com>
2020-09-08 13:33:08 -0700

    added data, worked on modify sit. Mostly working, needs tests

--- R/shaw_modify_sit.R
+++ R/shaw_modify_sit.R
@@ -1,14 +1,12 @@
-# Testing:
-model_dir <- "/Volumes/research_storage2/arctic/arctic_point_modeling"
-existing_site_file <- paste0(model_dir, "/SHAW/test_shaw/test_shaw.sit")
-
-
 #' Function to build SHAW input files based on parameters
 #'
 #' This function allows you to build SHAW files based on desired parameters.
 #' Most parameters default to NULL, and nothing in the site file changes. If you change any of the parameters from NULL, the parameter will change in the new site file.
 #' @import dplyr
 #' @import utils
+#' @import magrittr
+#' @import lubridate
+#' @import purrr
 
 #' @param model_dir This is the directory where all inputs will be written.
 #' @param existing_site_file The existing site file
@@ -33,24 +31,24 @@ existing_site_file <- paste0(model_dir, "/SHAW/test_shaw/test_shaw.sit")
 # 4. Add errors and warnings.
 
 shaw_modify_sit <- function(model_dir,
-                            existing_site_file = paste0(model_dir, "/", basename(model_dir), ".sit")
-                     #        start = NULL, start_hour = NULL, end = NULL, lat_deg = NULL, lat_min = NULL, slope = NULL, aspect = NULL, elev = NULL,
-                     # soils_df = NULL, nres = NULL, roughness = NULL, measurement_height = NULL, ponding = NULL, itype = NULL, pintrcp = NULL, xangle = NULL,
-                     # canalb = NULL, tcrit = NULL, min_stomatal_resistance = NULL, stomatal_exp = NULL, critical_leaf_water = NULL, leaf_resist = NULL, root_resist = NULL,
-                     # plant_height = NULL, dchar = NULL, clumping = NULL, biomass = NULL, LAI = NULL, rooting = NULL,
-                     # residue_thickness = NULL, residue_weight = NULL, residue_fraction = NULL,
-                     # residue_albedo = NULL, lower_bc_temp = NULL, nsalt = NULL, iwrc = NULL,
-                     # ivlcbc  = NULL, itmpbc  = NULL, dry_soil_albedo  = NULL, soil_albedo_exponent = NULL,
-                     # residue_coefficient = NULL, residue_krb = NULL,
-                     # isnotemp = NULL, snotemp = NULL, snow_roughness = NULL,
-                     # Kst = NULL, Tlower = NULL, Tupper = NULL, Topt = NULL, Kvpd = NULL, r = NULL,
-                     # mcanflg = NULL, istomate = NULL, canma = NULL, canmb = NULL, wcandt = NULL, hrnoon = NULL,
-                     # nplants = NULL, nsp = NULL, nsolutes = NULL, error_tol = NULL, time_step = NULL,
-                     # nrchang = NULL, gmcdt = NULL,
-                     # debugging_seq = NULL,
-                     # gro_file = NULL,
-                     # run_name = NULL,
-                     # soils_df = NULL
+                            existing_site_file = paste0(model_dir, "/", basename(model_dir), ".sit"),
+                            start = NULL, start_hour = NULL, end = NULL,
+                          lat_deg = NULL, lat_min = NULL, slope = NULL, aspect = NULL, elev = NULL,
+                     soils_df = NULL, nres = NULL, roughness = NULL, measurement_height = NULL, ponding = NULL, itype = NULL, pintrcp = NULL, xangle = NULL,
+                     canalb = NULL, tcrit = NULL, min_stomatal_resistance = NULL, stomatal_exp = NULL, critical_leaf_water = NULL, leaf_resist = NULL, root_resist = NULL,
+                     plant_height = NULL, dchar = NULL, clumping = NULL, biomass = NULL, LAI = NULL, rooting = NULL,
+                     residue_thickness = NULL, residue_weight = NULL, residue_fraction = NULL,
+                     residue_albedo = NULL, lower_bc_temp = NULL, nsalt = NULL, iwrc = NULL,
+                     ivlcbc  = NULL, itmpbc  = NULL, dry_soil_albedo  = NULL, soil_albedo_exponent = NULL,
+                     residue_coefficient = NULL, residue_krb = NULL,
+                     isnotemp = NULL, snotemp = NULL, snow_roughness = NULL,
+                     Kst = NULL, Tlower = NULL, Tupper = NULL, Topt = NULL, Kvpd = NULL, r = NULL,
+                     mcanflg = NULL, istomate = NULL, canma = NULL, canmb = NULL, wcandt = NULL, hrnoon = NULL,
+                     nplants = NULL, nsp = NULL, nsolutes = NULL, error_tol = NULL, time_step = NULL,
+                     nrchang = NULL, gmcdt = NULL,
+                     debugging_seq = NULL,
+                     gro_file = NULL,
+                     run_name = NULL
                      ){ # begin function
 
   # read site file
@@ -71,12 +69,11 @@ shaw_modify_sit <- function(model_dir,
   }
 
   # Get relevant flags that determine locations of parameters:
-  nplants <- as.numeric(site_lines[[4]][1]) # number of plants
-  nr <- as.numeric(site_lines[[4]][3]) # number of residue nodes.
-  ns <- as.numeric(site_lines[[4]][4]) # number of soil nodes.
-  mcanflg <- as.numeric(site_lines[[6]][1]) # growth type
-  istomate <- as.numeric(site_lines[[6]][2]) # stomatal conductance flag
-  max_line <- length(site) # max number of lines
+  if(is.null(nplants)){nplants <- as.numeric(site_lines[[4]][1])} # number of plants
+  if(is.null(nr)){nr <- as.numeric(site_lines[[4]][3])}  # number of residue nodes.
+  if(is.null(ns)){ns <- as.numeric(site_lines[[4]][4])} # number of soil nodes.
+  if(is.null(mcanflg)){mcanflg <- as.numeric(site_lines[[6]][1])} # growth type
+  if(is.null(istomate)){istomate <- as.numeric(site_lines[[6]][2])} # stomatal conductance flag
 
   if(nplants > 0){
     par_info$Line_number[par_info$Line_letter == "f"] <- 6:(5 + nplants) # F series
@@ -125,9 +122,8 @@ shaw_modify_sit <- function(model_dir,
     j3_1 <- j2 + 1 # j3_1 is first line of soils. not included in SHAW_parameters csv.
   } else {j3_1 <- j1 + 1}
 
-
     # Make some warnings for cases we haven't made yet. -------------
-  if(nsp != 0 & !is.null(nsp)){
+  if(!is.null(nsp) && nsp > 0){
     stop("NSP is required to be 0; greater values have not been implemented at this time.
                     Suggest starting in summer so NSP can reasonably be 0 for now.")
   }
@@ -136,23 +132,21 @@ shaw_modify_sit <- function(model_dir,
     stop("This function is not yet set up to deal with MCANFLG values > 1.")
   }
 
-  if(nsalt > 0){
+  if(!is.null(nsalt) && nsalt > 0){
     stop("This function isn't yet set up to deal with solutes.")
   }
 
-  if(nrchang == 1){
+  if(!is.null(nrchang) && nrchang == 1){
     stop("This function isn't set up to deal with changing residue layers.")
   }
 
-  # Function to either extract variables from site file orget from function.
+  # Function to either extract variables from site file or get from function arguments.
   fun_new_value <- function(x){ # x is a data frame with 1 row.
     var <- x$R_variable[1]
     if(is.null(get(var))){ # if a variable was not given in the function...
       linex <- x$Line_number[[1]]
       positionx <- x$Position[1]
-      if(length(linex) > 1){x$new_value[[1]] <- site_lines[[linex]][positionx]
-      }else {x$new_value[[1]] <- site_lines[linex][positionx]} # use data from site file
-
+      x$new_value[[1]] <- unlist(purrr::map(site_lines[linex], positionx)) # use data from site file
     } else {
       x$new_value[[1]] <- get(var) # use data given in function
     }
@@ -161,17 +155,13 @@ shaw_modify_sit <- function(model_dir,
 
   # Replace variables in par_info$new_value
   par_info2 <- par_info %>%
+    filter(!is.na(Line_number)) %>%
     mutate(new_value = as.list(new_value)) %>%
     split(.$R_variable) %>%
     purrr::map(fun_new_value) %>%
     bind_rows()
 
-  new_values <- par_info$new_value
-  names(new_values) <- par_info$R_variable
-
   # Write site file.---------------
-  if(!is.null(run_name)){site[1] <- run_name}  # Line A.
-
   # Line B # gets special treatment because of the way these are passed to function.
   if(!is.null(start)){
     start <- lubridate::ymd(start)
@@ -182,105 +172,36 @@ shaw_modify_sit <- function(model_dir,
     site_lines[[2]][4:5] <- paste(lubridate::yday(end), lubridate::year(end))
   }
 
-  site[3] <- paste(lat_deg, lat_min, slope, aspect, hrnoon, elev) # Line C
-
-  site[4] <- paste(nplants, nsp, nres, nrow(soils_df), nsolutes, error_tol, time_step,
-                   paste(debugging_seq, collapse = " "),
-                   collapse = " ") # Line D.
-
-  site[5] <- paste(roughness, measurement_height, ponding, sep = " ") # Line E
-
-  site[6] <- paste(mcanflg, istomate, canma, canmb, wcandt) # Line F.
-
-  # Define line numbers for F-1 to F1-NPlants.
-  f1_lines <- 7:(7 + nplants-1)
-  for(i in 1:length(f1_lines)){
-    site[f1_lines[i]] <- paste(itype[i], pintrcp[i], xangle[i], canalb[i], tcrit[i],
-                               min_stomatal_resistance[i], stomatal_exp[i], critical_leaf_water[i], leaf_resist[i], root_resist[i])
-  }
-
-  # Fa series of lines: (Stewart-Jarvis functions.)
-  max_line <- length(site) # determine how many lines we have so far.
-  if(istomate == 2){
-    fa_lines <- (max_line + 1):(max_line + nplants)
-    for(i in 1:fa_lines){
-      site[fa_lines[i]] <- paste(Kst[i], Tlower[i], Tupper[i], Topt[i], Kvpd[i], r[i], 0, 0, sep = " ")
-    } # end looping through fa_lines
-  } # end if Istomate = 2. If Istomate = 1, nothing to do here.
-
-  # F0 series of lines -- info if MCANFLG = 0, no growth.
-  max_line <- length(site)
-  if(mcanflg == 0){
-    f0_lines <- (max_line + 1):(max_line + nplants)
-    for(i in 1:f0_lines){
-      site[f0_lines[i]] <- paste(plant_height[i], dchar[i], clumping[i], biomass[i], LAI[i], rooting[i])
-    } # end looping through f0 lines
-  } # end if mcanflg = 0
-
-  # F1 series if mcanflg = 1
-  max_line <- length(site)
-  if(mcanflg == 1){
-    f1_lines <- (max_line + 1):(max_line + nplants)
-    for(i in 1:f1_lines){
-      site[f1_lines[i]] <- gro_file[i]
+  # Write other lines using data from par_info2
+  site_lines2 <- site_lines # make a copy to modify
+  for(i in 1:nrow(par_info2)){
+    line_numbers <- par_info2$Line_number[[i]]
+    position <- par_info2$Position[[i]]
+    new_values <- par_info2$new_value[[i]]
+    for(j in 1:length(line_numbers)){
+      site_lines2[[line_numbers[j]]][position] <- new_values[j]
     }
   }
 
-  # Line G. (snow)
-  site[length(site) + 1] <- paste(isnotemp, snotemp, snow_roughness, 0, collapse = " ")
+  site_lines2 <- lapply(site_lines2, paste, collapse = " ")
 
-  # Line H. (residue setup info info)
-  if(nres != 0){
-    site[length(site) + 1] <- paste(nrchang, gmcdt, collapse = " ")
+  # Now need to add soils df.
+  if(!is.null(soils_df)){
+    soils_lines <- soils_df %>%
+      split(.$depth) %>%
+      purrr::map_chr(~str_c(as.numeric(.[1,]), collapse = "\t"))
+
+    site_lines2[[j3_1:length(site_lines2)]] <- soils_lines
   }
 
-  # Line H1 (residue details)
-  if(nres != 0){
-    site[length(site) + 1] <- paste(residue_thickness, residue_weight, residue_fraction,
-                                    residue_albedo, residue_coefficient, residue_krb)
-  }
+  site_lines2 <- unlist(site_lines2)
 
-  # Line J1 (setup information about soils.)
-  site[length(site) + 1] <- paste(ivlcbc, itmpbc, dry_soil_albedo, soil_albedo_exponent, iwrc, 0, collapse = " ")
-
-  # Line J2 (lower bounary condition soil temperature)
-  if(itmpbc == 1){
-    site[length(site) + 1] <- lower_bc_temp # TSAVG in SHAW.
-  }
-
-  # Soils DF:
-  if(iwrc == 1){ # Campbell equation.
-    soils_df <- soils_df[,c("depth", "sand", "silt", "clay", "rock", "organic", "bulk_density", "ksat", "klat", "air_entry",
-                            "theta_sat", "b", "asalt", "dispersion_coef")]
-  }
-  if(iwrc == 2){ # Brooks-Corey
-    soils_df <- soils_df[,c("depth", "sand", "silt", "clay", "rock", "organic", "bulk_density", "ksat", "klat", "air_entry",
-                            "theta_sat", "brooks_corey_lambda", "theta_res", "l", "asalt", "dispersion_coefficient")]
-  }
-  if(iwrc == 3){ # van Genuchten
-    soils_df <- soils_df[,c("depth", "sand", "silt", "clay", "rock", "organic", "bulk_density", "ksat", "klat", "air_entry",
-                            "theta_sat", "n", "theta_res", "l", "alpha")]
-  }
-
-  soils_lines <- apply(soils_df, MARGIN = 1, FUN = paste, collapse = "\t") # make soils lines.
-  site[(length(site) + 1):(length(site) + length(soils_lines))] <- soils_lines
-
-  writeLines(site, site_file)
-
-
-  return(site)
+  return(site_lines2)
 
 }  # end function
-
-# Notes on line numbers:
-# 1. Line A (run timing)
-# 2. Line B (start/end)
-# 3. Line C (lat, slope aspect.)
-# 4. Line D - NPLANT, NR, NS, etc. Right now, we're not including snow and changeable residue nodes.
-# But NPLANT and NSOIL affect the run.
-# 5. Line E (roughness, measurement height)
-# 6. Line F (MCANFLG, ISTOMATE, coefficients)
-# 7-7 + NPlant
+# #
+# new_site <- shaw_modify_sit(model_dir = "/Volumes/research_storage2/arctic/arctic_point_modeling/SHAW/test_shaw",
+#                             lat_deg = 65)
 
 
 
